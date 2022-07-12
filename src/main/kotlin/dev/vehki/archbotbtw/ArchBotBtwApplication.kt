6f24a06500401171995146079349e84ef290042a
Sergiu Ababii <l33tbird@gmail.com>
2022-07-12 15:37:36 +0300

    Initial Commit

---
+++ src/main/kotlin/dev/vehki/archbotbtw/ArchBotBtwApplication.kt
@@ -0,0 +1,11 @@
+package dev.vehki.archbotbtw
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ArchBotBtwApplication
+
+fun main(args: Array<String>) {
+	runApplication<ArchBotBtwApplication>(*args)
+}
